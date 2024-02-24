7d8282518a00099ab52cb3b41a3e4dd0a7dba9ec
nefelipar <marianep@csd.auth.gr>
2024-02-24 22:22:08 +0200

    fifth question with kmeans, dbscan, minibatchkmeans, bisectingkmeans

--- analytics/clustering/clustering.py
+++ analytics/clustering/clustering.py
@@ -1,11 +1,10 @@
 import json
-
 import pandas as pd
 from time import time
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans, BisectingKMeans, DBSCAN
 from sklearn.metrics import silhouette_score
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def perform_lsi(speeches, num_components):
@@ -37,26 +36,47 @@ def perform_lsi(speeches, num_components):
 
     return transformed_tfispeeches
 
+start_time = time()
 
+start_time_pross = time()
 # Initialize the dataset path
 dataset_path = '../../data/Processed_Greek_Parliament.csv'
 # Read the dataset path
 speeches = pd.read_csv(dataset_path)
 
 lsi_speeches = perform_lsi(speeches, 100)
+end_time_pross = time()
+print(f"Ο χρόνος εκτέλεσης του pross είναι: {end_time_pross - start_time_pross} δευτερόλεπτα")
+
 
 start_time_cluster = time()
 
 # Εφαρμογή του αλγορίθμου k-Means
 num_clusters = 100
-kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=300)
-speeches['Cluster'] = kmeans.fit_predict(lsi_speeches)
+cluster_method = 'DBSCAN'
+
+if cluster_method == 'KMeans':
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    speeches['Cluster'] = kmeans.fit_predict(lsi_speeches)
+elif cluster_method == 'DBSCAN':
+    dbscan = DBSCAN(eps=0.9, min_samples=100, metric='cosine', n_jobs=-1)
+    speeches['Cluster'] = dbscan.fit_predict(lsi_speeches)
+elif cluster_method == 'MiniBatchKMeans':
+    mbkmeans = MiniBatchKMeans(n_clusters=num_clusters, random_state=42)
+    speeches['Cluster'] = mbkmeans.fit_predict(lsi_speeches)
+elif cluster_method == 'BisectingKMeans':
+    bkmeans = BisectingKMeans(n_clusters=num_clusters, random_state=42)
+    speeches['Cluster'] = bkmeans.fit_predict(lsi_speeches)
+else:
+    print(f"Άγνωστη μέθοδος ομαδοποίησης: {cluster_method}")
+
+
 end_time_cluster = time()
 print(f"Ο χρόνος εκτέλεσης του clustering είναι: {end_time_cluster - start_time_cluster} δευτερόλεπτα")
 
 start_time_silhouette = time()
 # Υπολογισμός του Silhouette Score
-silhouette_avg = silhouette_score(lsi_speeches, speeches['Cluster'], metric='cosine')
+silhouette_avg = silhouette_score(lsi_speeches, speeches['Cluster'], metric='cosine', sample_size=100000)
 print(f"Το Silhouette Score είναι: {silhouette_avg}")
 end_time_silhouette = time()
 print(f"Ο χρόνος εκτέλεσης του silhouette είναι: {end_time_silhouette - start_time_silhouette} δευτερόλεπτα")
@@ -68,7 +88,14 @@ start_time_write = time()
 unique_clusters = speeches['Cluster'].nunique()
 for cluster_id in range(unique_clusters):
     indices = speeches[speeches['Cluster'] == cluster_id]['ID'].tolist()
-    cluster_info = {f'Cluster {cluster_id}': {'SpeechId': indices}}
+    num_speeches = len(indices)
+    cluster_info = {
+        f'Cluster {cluster_id}': {
+            'ClusterSize': num_speeches,
+            'SpeechId': indices
+        }
+    }
+    print('cluster ', cluster_id, '  size:', len(indices))
     results.update(cluster_info)
 
 # Αναπροσαρμογή για την εμφάνιση των IDs δίπλα-δίπλα
@@ -79,7 +106,7 @@ for cluster_id, cluster_info in results.items():
 output_file_path = 'output.json'
 with open(output_file_path, 'w') as json_file:
     json.dump(results, json_file, indent=2)
-end_time_write = time.time()
+end_time_write = time()
 print(f"Ο χρόνος εκτέλεσης του write είναι: {end_time_write - start_time_write} δευτερόλεπτα")
 
 # Εκτύπωση του χρόνου εκτέλεσης
